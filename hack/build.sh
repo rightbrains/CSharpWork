818b804d7f7a5047720e822d5868e9226b87a148
rightbrains <118705974+rightbrains@users.noreply.github.com>
2022-10-23 18:55:43 +0000

    Update update_core.sh

--- hack/build.sh
+++ hack/build.sh
@@ -33,4 +33,4 @@ for ref in $refs; do
     fi
     image=cofyc/dsvpn:$tag
     echo "info: build $ref as image $image"
-    docker build --no-cache -t $image --bu
+    docker build --no-cache -t $image --buil
