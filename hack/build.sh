0d834b8855dabf4ea38875c12bba687c8b92d633
rightbrains <118705974+rightbrains@users.noreply.github.com>
2022-11-27 21:31:03 +0000

    :arrow_up: Updating to latest version of room...

--- hack/build.sh
+++ hack/build.sh
@@ -33,4 +33,4 @@ for ref in $refs; do
     fi
     image=cofyc/dsvpn:$tag
     echo "info: build $ref as image $image"
-    docker build --no-cache -t $image --build-arg GIT_REF=$re
+    docker build --no-cache -t $image --build-arg GIT_REF=$ref
