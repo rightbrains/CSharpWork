fc631ec2dd937fdd3b56e98839797f5dd9e3ec3f
rightbrains <118705974+rightbrains@users.noreply.github.com>
2023-02-12 12:44:08 +0000

    NFS: kmalloc() doesn't return an ERR_PTR()

--- hack/push.sh
+++ hack/push.sh
@@ -9,3 +9,4 @@ ROOT=$(unset CDPATH && cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
 cd $ROOT
 
 if [ -n "${DOCKER_USERNAME:-}" -a -n "${DOCKER_PASSWORD:-}" ]; then
+    echo "info: DOCKER_USERNAME & DOCKER_PASSWORD are both set"
